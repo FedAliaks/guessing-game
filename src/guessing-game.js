3c5bb903b9c01fd2bcc3c469963860eb723f6175
Aliaksandr Fedukovich <1632637@mail.ru>
2023-02-20 10:57:34 +0300

    feat: add solution task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+   }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        let result = Math.ceil((this.min + this.max) / 2);
+        console.log('guess: '+ result);
+        return result;
     }
 
     lower() {
-
+        this.max = Math.round((this.min + this.max) / 2);
+        console.log('lower: ');
     }
 
     greater() {
-
+        this.min = Math.round((this.min + this.max) / 2);
+        console.log('greater: ');
     }
 }
 
